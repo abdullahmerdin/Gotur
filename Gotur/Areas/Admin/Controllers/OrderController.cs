e631e425dbad39fcf10167551236377b0869a7a2
Abdullah Merdin <abdullahmerdin@gmail.com>
2022-05-21 11:07:33 +0300

    Order view,controller and viewmodel

---
+++ Gotur/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using Gotur.Data.Repository.IRepository;
+using Gotur.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gotur.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class OrderController : Controller
+    {
+        //Dependency Injection
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderVM OrderVm { get; set; }
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork=unitOfWork;
+        }
+        
+
+        public IActionResult Index()
+        {
+            //Tamamlanmamış tüm siparişleri getir
+            var orderList = _unitOfWork.OrderProduct.GetAll(x=>x.OrderStatus!="Teslim Edildi");
+            return View(orderList);
+        }
+    }
+}
